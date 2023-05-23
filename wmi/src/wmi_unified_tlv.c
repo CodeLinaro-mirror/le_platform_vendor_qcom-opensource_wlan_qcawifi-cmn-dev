5fa11fdabcf28575ec31f113009ae79bac36a3d9
Miaoqing Pan <quic_miaoqing@quicinc.com>
2023-05-23 10:05:47 +0800

    qcacmn: Update flag for connectionless feature
    Update flag for connectionless feature.
    
    CRs-Fixed: 3505638
    Change-Id: Iaca4aa19ebee0b13d5e3f3c066ddae2bcb51a7e7

--- wmi/src/wmi_unified_tlv.c
+++ wmi/src/wmi_unified_tlv.c
@@ -1108,7 +1108,7 @@ static QDF_STATUS send_vdev_start_cmd_tlv(wmi_unified_t wmi_handle,
 		cmd->flags |= WMI_UNIFIED_VDEV_START_HIDDEN_SSID;
 
 	if (req->special_vdev_mode)
-		cmd->flags |= WMI_UNIFIED_VDEV_START_SPECIAL_AP_BCAST_ENABLED;
+		cmd->flags |= WMI_UNIFIED_VDEV_START_CONNECTIONLESS_ALLOWED;
 
 	cmd->flags |= WMI_UNIFIED_VDEV_START_LDPC_RX_ENABLED;
 	cmd->num_noa_descriptors = req->num_noa_descriptors;
